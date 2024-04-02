c5ab27546149e8c39af68e87f1427fafa701a11e
dev <nikitindmitry7@gmail.com>
2024-04-02 15:58:21 +0300

    v 0.1.7. - fix reg frontend bug

--- adsynch/main/views.py
+++ adsynch/main/views.py
@@ -16,19 +16,25 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
+            # username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password1']
             # Создаем пользователя
-            username = translit(name.lower(), reversed=True)  # Транслитерация имени в lowercase
+            username = translit(name, reversed=True)  # Транслитерация имени в lowercase
             username += '.' + str(uuid.uuid4().hex)[:6]  # Добавляем уникальный идентификатор
 
-            user = User.objects.create_user(name, username, email, password)
+            user = User.objects.create_user(username, email, password)
+            print('username', username)
             user.save()
             messages.success(request, 'Пользователь успешно зарегистрирован!')
+            print('1')
             return redirect('index')  # перенаправить на вашу главную страницу
         else:
             # Если форма невалидна, передаем ошибки в шаблон
             messages.error(request, 'Пожалуйста, исправьте ошибки в форме.')
+            print(form.errors.as_data())
+
     else:
         form = RegistrationForm()
+        print('3')
     return render(request, 'main/index.html', {'form': form})
