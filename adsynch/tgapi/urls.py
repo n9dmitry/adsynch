46f32927e2de2ecfc8d0971042a2c858d1286883
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2024-04-25 23:17:51 +0300

    v 0.1.0 views, serializer works!

--- adsynch/tgapi/urls.py
+++ adsynch/tgapi/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import CarAdView, RealtyAdView, JobAdView
+
 
 urlpatterns = [
     # path("user_data/", views.bot_webhook),  # Обратите внимание на двоеточие после views
-    path("car_ad/", views.car_webhook),
-    path("realty_ad/", views.realty_webhook),
-    path("job_ad/", views.job_webhook),
+    path("car_ad/", CarAdView.as_view(), name='car_ad'),
+    path("realty_ad/", RealtyAdView.as_view(), name='realty_ad'),
+    path("job_ad/", JobAdView.as_view(), name='job_ad'),
+    # path("realty_ad/", views.realty_webhook),
+    # path("job_ad/", views.job_webhook),
 
 
     # path("myads/", views.RealtyListAPIView.as_view()),  # Обратите внимание на двоеточие после views
