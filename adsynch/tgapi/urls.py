a052c9dec7f0dbeb21dfce43e44c6a48fcbc0ea4
dev <nikitindmitry7@gmail.com>
2024-05-21 15:53:31 +0300

    0.1.5. check_user

--- adsynch/tgapi/urls.py
+++ adsynch/tgapi/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from .views import CarAdView, RealtyAdView, JobAdView
+from .views import CarAdView, RealtyAdView, JobAdView, check_user
 
 urlpatterns = [
     # path("user_data/", views.bot_webhook),  # Обратите внимание на двоеточие после views
     path("car_ad/", CarAdView.as_view()),
     path("realty_ad/", RealtyAdView.as_view()),
     path("job_ad/", JobAdView.as_view()),
-
+    path('check_user/<str:username>/', check_user, name='check_user'),
 
     # path("myads/", views.RealtyListAPIView.as_view()),  # Обратите внимание на двоеточие после views
     # path('', views.display_cars, name='cars'),
