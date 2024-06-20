7680efe834711744d8497511000460ccd145c72c
EnotikProger <123252340+EnotikProger@users.noreply.github.com>
2024-06-20 09:56:57 +0300

    django site v1.9.1

--- adsynch/search/urls.py
+++ adsynch/search/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search_view, name='search_results'),
+    path('', views.search_view, name='search_results'),
     # Добавьте другие пути при необходимости
 ]
