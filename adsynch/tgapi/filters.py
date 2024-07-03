ae3f94846e54d025c1d82f9ec08b51ad07a6faec
dev <nikitindmitry7@gmail.com>
2024-07-03 13:22:54 +0300

    v0.5.0. global filters changes

--- adsynch/tgapi/filters.py
+++ adsynch/tgapi/filters.py
@@ -2,45 +2,64 @@ import django_filters
 from .models import CarAd, RealtyAd, JobAd
 
 
-class CarAdFilter(django_filters.FilterSet):
-    price_min = django_filters.NumberFilter(field_name="car_price", lookup_expr='gte')
-    price_max = django_filters.NumberFilter(field_name="car_price", lookup_expr='lte')
-    year_min = django_filters.NumberFilter(field_name="car_year", lookup_expr='gte')
-    year_max = django_filters.NumberFilter(field_name="car_year", lookup_expr='lte')
-    mileage_min = django_filters.NumberFilter(field_name="car_mileage", lookup_expr='gte')
-    mileage_max = django_filters.NumberFilter(field_name="car_mileage", lookup_expr='lte')
+# основной фильтр сортировки
+class BaseFilterSet(django_filters.FilterSet):
+    order_by = django_filters.OrderingFilter(
+        fields=(
+            ('date_published', '-date_published'),
+            ('views', '-views'),
+            ('price', '-car_price'),
+        ),
+        field_labels={
+            'date_published': 'Новинки',
+            'views': 'Просмотры',
+            'price': 'Цена',
+        },
+        label='Сортировка',
+        choices=(
+            ('date_published', 'Новинки'),
+            ('-date_published', 'Новинки (по убыванию)'),
+            ('views', 'Просмотры (минимум)'),
+            ('-views', 'Просмотры (максимум)'),
+            ('price', 'Цена (дешевые)'),
+            ('-price', 'Цена (дорогие)'),
+        )
+    )
+class CarAdFilter(BaseFilterSet, django_filters.FilterSet, ):
+    price_min = django_filters.NumberFilter(field_name="car_price", label="Цена Min", lookup_expr='gte')
+    price_max = django_filters.NumberFilter(field_name="car_price", label="Цена Max", lookup_expr='lte')
     brand = django_filters.ChoiceFilter(
         field_name="car_brand",
+        label="Бренд",
         choices=[(brand, brand) for brand in CarAd.objects.values_list('car_brand', flat=True).distinct()]
     )
     model = django_filters.ChoiceFilter(
         field_name="car_model",
+        label="Модель",
         choices=[(car_model, car_model) for car_model in CarAd.objects.values_list('car_model', flat=True).distinct()]
     )
 
     condition = django_filters.ChoiceFilter(
         field_name="car_condition",
+        label="Состояние",
         choices=[(car_condition, car_condition) for car_condition in
                  CarAd.objects.values_list('car_condition', flat=True).distinct()]
     )
+    year_min = django_filters.NumberFilter(field_name="car_year", label="Год Min", lookup_expr='gte')
+    year_max = django_filters.NumberFilter(field_name="car_year", label="Год Max", lookup_expr='lte')
+    mileage_min = django_filters.NumberFilter(field_name="car_mileage", label="Пробег Min", lookup_expr='gte')
+    mileage_max = django_filters.NumberFilter(field_name="car_mileage", label="Пробег Min", lookup_expr='lte')
+
+
 
     class Meta:
         model = CarAd
-        fields = ['price_min', 'price_max', 'year_min', 'year_max', 'mileage_min', 'mileage_max', 'brand', 'model',
-                  'condition']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if 'brand' in self.data and self.data['brand']:
-            brand = self.data['brand']
-            self.filters['model'].extra.update({
-                'choices': [(model, model) for model in
-                            CarAd.objects.filter(car_brand=brand).values_list('car_model', flat=True).distinct()]
-            })
+        fields = ['order_by', 'price_min', 'price_max', 'brand', 'model',
+                  'condition', 'year_min', 'year_max', 'mileage_min', 'mileage_max', ]
 
 
 # Пытаюсь сделать остальные категории
-class RealtyAdFilter(django_filters.FilterSet):
+class RealtyAdFilter(BaseFilterSet, django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="price", label="Цена Min", lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name="price", label="Цена Max", lookup_expr='lte')
     realty_deal = django_filters.ChoiceFilter(
@@ -61,9 +80,6 @@ class RealtyAdFilter(django_filters.FilterSet):
         choices=[(realty_commercial_type, realty_commercial_type) for realty_commercial_type in
                  RealtyAd.objects.values_list('realty_commercial_type', flat=True).distinct()]
     )
-    # realty_deal = django_filters.CharFilter(field_name="realty_deal")
-    # realty_type = django_filters.CharFilter(field_name="realty_type")
-    # realty_commercial_type = django_filters.CharFilter(field_name="realty_commercial_type")
     realty_square_min = django_filters.NumberFilter(field_name="realty_square", label="Площадь Min", lookup_expr='gte')
     realty_square_max = django_filters.NumberFilter(field_name="realty_square", label="Площадь Max", lookup_expr='lte')
     realty_rooms_min = django_filters.NumberFilter(field_name="realty_rooms", label="Комнат Max", lookup_expr='gte')
@@ -71,22 +87,13 @@ class RealtyAdFilter(django_filters.FilterSet):
 
     class Meta:
         model = RealtyAd
-        fields = ['price_min', 'price_max', 'realty_deal', 'realty_type', 'realty_commercial_type', 'realty_square_min',
+        fields = ['order_by', 'price_min', 'price_max', 'realty_deal', 'realty_type', 'realty_commercial_type', 'realty_square_min',
                   'realty_square_max',
                   'realty_rooms_min', 'realty_rooms_max']
 
-    #     Тут надо будет переделать если коммерческий тип недвижимости
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     if 'brand' in self.data and self.data['brand']:
-    #         brand = self.data['brand']
-    #         self.filters['model'].extra.update({
-    #             'choices': [(model, model) for model in
-    #                         CarAd.objects.filter(car_brand=brand).values_list('car_model', flat=True).distinct()]
-    #         })
 
 
-class JobAdFilter(django_filters.FilterSet):
+class JobAdFilter(BaseFilterSet, django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="price", label="Зарплата от", lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name="price", label="Зарплата до", lookup_expr='lte')
     job_category = django_filters.ChoiceFilter(
@@ -97,4 +104,4 @@ class JobAdFilter(django_filters.FilterSet):
 
     class Meta:
         model = JobAd
-        fields = ['price_min', 'price_max', 'job_category']
+        fields = ['order_by', 'price_min', 'price_max', 'job_category']
